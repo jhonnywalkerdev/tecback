e28af0785dba6a7897cc2f61bfb57ae5ca782c9c
den-duar <2021211510079@iesp.edu.br>
2022-12-04 15:04:41 -0300

    Retirada de codigos sem uso

--- src/main/java/br/iesp/edu/api/entity/Genero.java
+++ src/main/java/br/iesp/edu/api/entity/Genero.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+
 
 @Getter
 @Setter
